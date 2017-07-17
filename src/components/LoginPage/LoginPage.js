d3cacd73b8b54ee3a327c4a8783a60f3a2f84de8
loren pabst <lorenpabst@gmail.com>
2017-07-17 11:38:24 -0600

    built functions for login page front end

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -3,18 +3,53 @@ import './LoginPage.css';
 
 
 class LoginPage extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.authorizeLogin = this.authorizeLogin.bind(this);
+    this.updateUsername = this.updateUsername.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+  }
+
+  authorizeLogin(){
+    axios.post('/api/login', {
+      "username": this.state.username,
+      "password": this.state.password
+    })
+    .then( res => {
+      console.log(res);
+    })
+  }
+
+  updateUsername(e){
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  updatePassword(e){
+    this.setState({
+      password: e.target.value
+    })
+  }
 
   render() {
     return (
       <div className="login_page">
 
           <h2>Admin Login</h2>
-          
+
           <div className='login_modal'>
             <h4>USERNAME</h4>
-            <input placeholder='username' />
+            <input placeholder='username' onChange={ this.updateUsername }/>
             <h4>PASSWORD</h4>
-            <input placeholder='password' type='password' />
+            <input placeholder='password' type='password' onChange={ this.updatePassword } />
+            <button onClick={ this.authorizeLogin } >Login</button>
           </div>
 
       </div>
