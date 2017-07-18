d22b36ded5ad78bba2f631c162b354f02e058316
loren pabst <lorenpabst@gmail.com>
2017-07-18 14:02:08 -0600

    built out the endpoint functions for the user controller

--- server/userController.js
+++ server/userController.js
@@ -2,28 +2,70 @@ const app = require('./index.js');
 
 module.exports = {
 
-    getBookByTitleAlph: function(){
-
+    getBookByTitleAlph: function(req, res, next){
+        const db = req.app.get('db');
+        let title = req.params.title;
+        
+        db.getBookByTitleAlph([title])
+        .then( response => {
+            return res.status(200).json(response);
+        })
+        .catch( err => res.status(500).json(err) )
     },
 
-    getBookByTitleYear: function(){
+    getBookByTitleYear: function(req, res, next){
+        const db = req.app.get('db');
+        let title = req.params.title;
+        db.getBookByTitleYear([title])
+        .then( response => {
+            return res.status(200).json(response);
 
+        })
+        .catch( err => res.status(500).json(err) )
     },
 
-    getBookByAuthorAlph: function(){
+    getBookByAuthorAlph: function(req, res, next){
+        const db = req.app.get('db');
+        let author = req.params.author;
+        db.getBookByAuthorAlph([author])
+        .then( response => {
+            return res.status(200).json(response);
 
+        })
+        .catch( err => res.status(500).json(err) )
     },
 
-    getBookByAuthorYear: function(){
+    getBookByAuthorYear: function(req, res, next){
+        const db = req.app.get('db');
+        let author = req.params.author;
+        db.getBookByAuthorYear([author])
+        .then( response => {
+            return res.status(200).json(response);
 
+        })
+        .catch( err => res.status(500).json(err) )
     },
 
-    getBookBySeriesAlph: function(){
+    getBookBySeriesAlph: function(req, res, next){
+        const db = req.app.get('db');
+        let series = req.params.series;
+        db.getBookBySeriesAlph([series])
+        .then( response => {
+            return res.status(200).json(response);
 
+        })
+        .catch( err => res.status(500).json(err) )
     },
 
-    getBookBySeriesYear: function(){
+    getBookBySeriesYear: function(req, res, next){
+        const db = req.app.get('db');
+        let series = req.params.series;
+        db.getBookBySeriesYear([series])
+        .then( response => {
+            return res.status(200).json(response);
 
+        })
+        .catch( err => res.status(500).json(err) )
     },
 
 };
