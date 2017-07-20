0c575cbe642e0c537c72ae54b7925c8d3dcae60a
loren pabst <lorenpabst@gmail.com>
2017-07-20 15:52:19 -0600

    styling search results more

--- src/components/Home/Book/Book.js
+++ src/components/Home/Book/Book.js
@@ -6,7 +6,10 @@ class Book extends Component {
   render() {
     return (
         <div className='book_result_box'>
-
+          <h3>The Final Empire, 2006</h3>
+          <h4>Brandon Sanderson</h4>
+          <h4>Copies: 3</h4>
+          <h4>Available: 2</h4>
         </div>
     )
   }
